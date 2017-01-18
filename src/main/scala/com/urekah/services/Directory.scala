d80c6adf678b175f2096ef1bc96335310956b5bb
Javier Argote <javier@urekah.io>
2017-01-18 15:45:05 +0000

    Directory logic and scala tests for it.

--- src/main/scala/com/urekah/services/Directory.scala
+++ src/main/scala/com/urekah/services/Directory.scala
@@ -9,10 +9,26 @@ import akka.actor.{Actor, ActorRef, ActorLogging}
 
 class Directory(contacts: ActorRef, prefixes: ActorRef) extends Actor with ActorLogging {
   import Directory.Protocol._
+  import ContactManager.Protocol.Get
+  import Prefix.Protocol.{Search, SearchResult}
+
 
   def receive: Receive = {
-    case SearchByPrefix(prefix: String) => sender ! Unit
-    case SearchById(id: UUID[Contact]) => sender ! Unit
+    case SearchById(id: UUID[Contact]) => contacts ! Get(id)
+    case SearchByPrefix(prefix: String) => prefixes ! Search(prefix)
+    case SearchResult(prefix, _, data) =>
+      val results = data map {
+        case (id, name, contact) => s"${id.value.toString} -> $name"
+      }
+      context.parent ! results
+    case Contact(id, firstName, lastName, phoneNumber) =>
+      val msg = Seq(firstName, lastName, phoneNumber)
+        .foldLeft(s"${id.value.toString} ->") {
+        case (str: String, Some(field: Any)) =>
+          str + " " + field.toString
+        case (str: String, _) => str
+      }
+      context.parent ! msg
   }
 }
 
--- src/test/scala/com/urekah/services/DirectorySpec.scala
+++ src/test/scala/com/urekah/services/DirectorySpec.scala
@@ -4,7 +4,11 @@ package services
 import models.Contact
 import utils.UUID
 
-import akka.actor.{ActorRef, ActorSystem}
+import Directory.Protocol._
+import ContactManager.Protocol.Get
+import Prefix.Protocol.{Search, SearchResult}
+
+import akka.actor.{Actor, ActorRef, ActorSystem, Props}
 import akka.persistence._
 import akka.testkit.{TestKit, ImplicitSender, TestProbe}
 
@@ -23,50 +27,43 @@ class DirectorySpec
   }
 
   "Directory Service" must {
-    import Prefix.Protocol.Search
-    import ContactManager.Protocol.{Create, UpdateFirstName}
+    val anId = UUID.random[Contact]
+    val aContact = Contact(
+      id = anId,
+      firstName = "FooBaz".some,
+      lastName = "BarMan".some,
+      phoneNumber = "+0000000".some)
 
-    val aContactId = UUID.random[Contact]
-    val prefix = system.actorOf(Prefix.props)
-    val contact = system.actorOf(ContactManager.props(prefix, aContactId))
-
-    "#Setup tests" in {
-      // Create new prefix
-      prefix ! Search("foo")
-      expectMsg(Map.empty)
-
-      contact ! Create(aContactId)
-      expectMsg(Contact(id = aContactId))
-
-      contact ! UpdateFirstName(aContactId, firstName = "Foo Bar")
-      expectMsg(Contact(id = aContactId, firstName = "Foo Bar".some))
-
-      prefix ! Search("foo")
-      expectMsgPF() {
-        case msg: Map[_, _] =>
-          msg should contain (aContactId)
+    val probe = TestProbe()
+    val mockIndexAndContacts = system.actorOf(Props(new MockActor(aContact)))
+    val parent = system.actorOf(Props(new Actor {
+      val child = context.actorOf(Props(
+        new Directory(mockIndexAndContacts, mockIndexAndContacts)))
+      def receive = {
+        case msg if sender == child => probe.ref forward msg
+        case msg => child forward msg
       }
+    }))
+
+    "#SearchById" in {
+      probe.send(parent, SearchById(anId))
+      probe.expectMsg(anId.value.toString + " -> FooBaz BarMan +0000000")
     }
 
-    "#Search by UUID" in {
-      1 shouldBe 0
+    "#SearchByPrefix" in {
+      probe.send(parent, SearchByPrefix("foo"))
+      probe.expectMsg(List(anId.value.toString + " -> FooBazBarMan"))
     }
+  }
+}
 
-    // "#AddEntry" in {
-    //   prefix ! AddEntry("foo", aContactId, "Foo Bar", aContact)
-    //   expectMsgPF() {
-    //     case result: Map[_, _] =>
-    //       result.keys should contain (aContactId)
-    //       result.values should contain (("Foo Bar", aContact))
-    //   }
-    // }
-    //
-    // "#RemoveEntry" in {
-    //   prefix ! RemoveEntry("foo", aContactId)
-    //   expectMsgPF() {
-    //     case result: Map[_, _] =>
-    //       result.keys shouldNot contain (aContactId)
-    //   }
-    // }
+// class
+class MockActor(contact: Contact) extends Actor {
+  def receive: Receive = {
+    case Get(_) =>
+      sender ! contact
+    case cmd @ Search(prefix) =>
+      sender ! SearchResult(prefix, cmd,
+          Seq((contact.id, contact.firstName.get + contact.lastName.get, self)))
   }
 }
